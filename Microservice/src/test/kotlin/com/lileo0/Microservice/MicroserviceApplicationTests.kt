932a6caa70f91c5c87231a3dddbc7d9e01c8906f
Leo <Leo@DESKTOP-HBBE94K>
2023-11-06 20:54:32 +0100

    feat: added springboot project, added factors: 1,2,3,7 and 4 is ongoing

---
+++ Microservice/src/test/kotlin/com/lileo0/Microservice/MicroserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lileo0.Microservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
